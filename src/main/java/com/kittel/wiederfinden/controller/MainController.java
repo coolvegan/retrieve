1b7af249d517e198abe646a91928232c8f3bd9e8
Marco Kittel <marco.kittel@efrexx.com>
2021-08-31 21:45:07 +0200

    implemented item edit

--- src/main/java/com/kittel/wiederfinden/controller/MainController.java
+++ src/main/java/com/kittel/wiederfinden/controller/MainController.java
@@ -5,6 +5,7 @@ import com.kittel.wiederfinden.model.ItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -52,7 +54,7 @@ public class MainController {
     }
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST, headers = "Accept=application/x-www-form-urlencoded")
-    public String upload(Model model, @RequestParam(required = false) String name, @RequestParam(required = false) String location, @RequestParam(required = false) MultipartFile filepath) {
+    public String upload(Model model, @RequestParam(required = true) String name, @RequestParam(required = true) String location, @RequestParam(required = false) MultipartFile filepath) {
         String filePath = imgDirectory + "/" + filepath.getOriginalFilename();
         try {
             InputStream inputStream = filepath.getInputStream();
@@ -67,7 +69,7 @@ public class MainController {
         }
 
         Item item = new Item();
-        item.setFilepath(filePath);
+        item.setFilepath("/"+filePath);
         item.setLocation(location);
         item.setName(name);
 
@@ -79,5 +81,26 @@ public class MainController {
         return "redirect:/";
     }
 
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    String edit(Model model, @PathVariable("id") Long id){
+        if (id == null){
+            return "main";
+        }
+        Item item = itemRepo.getById(id);
+        model.addAttribute("edit", true);
+        model.addAttribute("item", item);
+        return "main";
+    }
 
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    String edit(@RequestParam(required = true) String name, @RequestParam(required = true) String location,  @RequestParam(required = true) Long id) {
+        Optional<Item> item = itemRepo.findById(id);
+        if (item.isEmpty()){
+            return "redirect:/";
+        }
+        item.get().setName(name);
+        item.get().setLocation(location);
+        itemRepo.save(item.get());
+        return "redirect:/";
+    }
 }
